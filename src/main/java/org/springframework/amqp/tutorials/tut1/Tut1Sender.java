ee3465b6e47077b970e8c6a0338cd75e3b252af9
user <user>
2020-02-05 08:43:00 +0000

    Added the sender.

---
+++ src/main/java/org/springframework/amqp/tutorials/tut1/Tut1Sender.java
@@ -0,0 +1,26 @@
+package org.springframework.amqp.tutorials.tut1;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * Tut1Sender
+ */
+public class Tut1Sender {
+
+	@Autowired
+	private RabbitTemplate template;
+
+	@Autowired
+	private Queue queue;
+
+	@Scheduled(fixedDelay = 1000, initialDelay = 500)
+	public void send() {
+		String message = "Hello world!";
+		this.template.convertAndSend(queue.getName(), message);
+		System.out.println(" [x] Send '" + message + "'");
+
+	}
+}
